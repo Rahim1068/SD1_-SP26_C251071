0d3a0b61dfcf79aac7b7ef35b107fe2c4ce44834
Rahim1068 <rahim101403@gmail.com>
2026-04-30 18:29:55 +0600

    Add files via upload

---
+++ Lab Report 1/Java_Data_Types.java
@@ -0,0 +1,19 @@
+package sd1_lab_report_1_c251071;
+
+public class Java_Data_Types {
+
+    public static void main(String[] args) {
+        int myNum = 5;
+        float myFloatNum = 5.99f;
+        char myLetter = 'E';
+        boolean myBool = true;
+        String myText = "Hello";
+
+        System.out.println(myNum);
+        System.out.println(myFloatNum);
+        System.out.println(myLetter);
+        System.out.println(myBool);
+        System.out.println(myText);
+    }
+
+}
