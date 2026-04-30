57250db82ada981338dd2f95786f193dfb2879cc
Rahim1068 <rahim101403@gmail.com>
2026-04-30 18:30:44 +0600

    Add files via upload

---
+++ Lab Report 1/Java_Operators.java
@@ -0,0 +1,15 @@
+package sd1_lab_report_1_c251071;
+
+public class Java_Operators {
+
+    public static void main(String[] args) {
+        int sum1 = 100 + 50;
+        int sum2 = sum1 + 250;
+        int sum3 = sum2 + sum2;
+
+        System.out.println(sum1);
+        System.out.println(sum2);
+        System.out.println(sum3);
+    }
+
+}
