ebc2eb97bf8ceb3fe8df93e85cf0de8d496e87d6
Rahim1068 <rahim101403@gmail.com>
2026-04-30 18:28:10 +0600

    Add files via upload

---
+++ Lab Report 1/Java_Comments.java
@@ -0,0 +1,11 @@
+package sd1_lab_report_1_c251071;
+
+public class Java_Comments {
+
+    public static void main(String[] args) {
+        /* The code below will print the words Hello IIUC 
+        to the screen, and it is amazing */
+        System.out.println("Hello IIUC");
+    }
+
+}
