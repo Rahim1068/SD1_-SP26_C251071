e442b0015a159afcee61305ba0cf1f2f3a4a631d
Rahim1068 <rahim101403@gmail.com>
2026-04-30 18:31:04 +0600

    Add files via upload

---
+++ Lab Report 1/Java_Strings.java
@@ -0,0 +1,12 @@
+package sd1_lab_report_1_c251071;
+
+public class Java_Strings {
+
+    public static void main(String[] args) {
+        String txt = "Hello rahim";
+
+        System.out.println(txt.toUpperCase());
+        System.out.println(txt.toLowerCase());
+    }
+
+}
