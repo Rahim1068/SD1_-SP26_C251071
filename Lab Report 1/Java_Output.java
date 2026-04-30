be430a0580fffcf4ee83b79cd805838bea21523f
Rahim1068 <rahim101403@gmail.com>
2026-04-30 18:27:33 +0600

    Add files via upload

---
+++ Lab Report 1/Java_Output.java
@@ -0,0 +1,11 @@
+package sd1_lab_report_1_c251071;
+
+public class Java_Output {
+
+    public static void main(String[] args) {
+        System.out.println(2);
+        System.out.println(3582);
+        System.out.println(500002);
+    }
+
+}
