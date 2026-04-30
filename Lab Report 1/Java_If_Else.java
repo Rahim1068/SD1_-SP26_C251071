57515dd0e847bf3e26639464753af7d3bb113237
Rahim1068 <rahim101403@gmail.com>
2026-04-30 18:35:49 +0600

    Add files via upload

---
+++ Lab Report 1/Java_If_Else.java
@@ -0,0 +1,16 @@
+package sd1_lab_report_1_c251071;
+
+public class Java_If_Else {
+
+    public static void main(String[] args) {
+        int x = 20;
+        int y = 20;
+
+        if (x == y) {
+            System.out.println("x is equal to y");
+        } else {
+            System.out.println("x is not equal to y");
+        }
+    }
+
+}
