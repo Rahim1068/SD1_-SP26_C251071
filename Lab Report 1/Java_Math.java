c8b10fd783f9c22c473969618dfea61d7f5b86f9
Rahim1068 <rahim101403@gmail.com>
2026-04-30 18:35:02 +0600

    Add files via upload

---
+++ Lab Report 1/Java_Math.java
@@ -0,0 +1,11 @@
+package sd1_lab_report_1_c251071;
+
+public class Java_Math {
+
+    public static void main(String[] args) {
+        System.out.println(Math.round(4.6));
+        System.out.println(Math.ceil(4.1));
+        System.out.println(Math.floor(4.9));
+    }
+
+}
