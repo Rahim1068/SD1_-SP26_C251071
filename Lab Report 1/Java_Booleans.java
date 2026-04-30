912e0b4f4acdafc966ea51951dd40b4c0d2c3365
Rahim1068 <rahim101403@gmail.com>
2026-04-30 18:35:30 +0600

    Add files via upload

---
+++ Lab Report 1/Java_Booleans.java
@@ -0,0 +1,14 @@
+package sd1_lab_report_1_c251071;
+
+public class Java_Booleans {
+
+    public static void main(String[] args) {
+        int x = 10;
+        int y = 9;
+
+        boolean isGreater = x > y;
+
+        System.out.println(isGreater);
+    }
+
+}
