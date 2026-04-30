19addb7ba6100ec93677c05b2540d77127de92ac
Rahim1068 <rahim101403@gmail.com>
2026-04-30 18:30:17 +0600

    Add files via upload

---
+++ Lab Report 1/Java_Type_Casting.java
@@ -0,0 +1,13 @@
+package sd1_lab_report_1_c251071;
+
+public class Java_Type_Casting {
+
+    public static void main(String[] args) {
+        int myInt = 9;
+        double myDouble = myInt;
+
+        System.out.println(myInt);
+        System.out.println(myDouble);
+    }
+
+}
