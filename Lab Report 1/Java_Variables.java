79e53bba3db284dcd1c1596c0c388dbf91fddc06
Rahim1068 <rahim101403@gmail.com>
2026-04-30 18:29:30 +0600

    Add files via upload

---
+++ Lab Report 1/Java_Variables.java
@@ -0,0 +1,11 @@
+package sd1_lab_report_1_c251071;
+
+public class Java_Variables {
+
+    public static void main(String[] args) {
+        int myNum = 15;
+        myNum = 20;
+        System.out.println(myNum);
+    }
+
+}
